1d142591b75df95c885ee34ff72bf4887d32cc22
William Marques <william.marques@dbserver.com.br>
2023-09-27 12:11:52 -0300

    DesafioBuscaNome Concluído

---
+++ src/DesafioBuscaNome.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DesafioBuscaNome {
+
+    public static void main(String[] args) {
+
+        String vetor[] = new String[3];
+        String nome, buscavetor;
+        boolean buscanome = false;
+        int p;
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("informe os nomes para o vetor de 10 posições");
+
+
+        for (p=0 ; p<vetor.length ; p++){
+            System.out.println("Informe um nome para a posição "+p);
+            vetor[p] = ler.next();
+        }
+        /**
+        for (p=0 ; p<vetor.length ; p++){
+            System.out.println("Vetor tem os seguintes nomes: " +p);
+        }*/
+
+        System.out.println("Informe um nome para buscar no vetor");
+        nome = ler.next();
+
+        for (p=0 ; p<vetor.length ; p++) {
+            buscavetor = vetor[p];
+            if(buscavetor.equals(nome)){
+                System.out.println("Nome encontrado no vetor na posição "+p);
+                buscanome = true;
+                break;
+            }else if (buscavetor != nome){
+                System.out.println("Nome não encontrado no vetor na posição " +p);
+                buscanome = false;
+            }
+        }
+
+        if(buscanome == true){
+            System.out.println("O nome digitado FOI encontrado no vetor");
+        }else
+            System.out.println("O nome digitado NÃO FOI encontrado no vetor");
+    }
+}
